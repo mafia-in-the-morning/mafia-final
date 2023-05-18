a0b6eccc135cccf01ca0267138c2a5ab16dc3874
kokojihye <jihyevivianako@gmail.com>
2023-05-19 02:48:09 +0900

    메인 클래스 수정

--- src/src/main/java/mafia_42/main/Main.java
+++ src/src/main/java/mafia_42/main/Main.java
@@ -14,7 +14,9 @@ import java.lang.*;
 import static mafia_42.game.Game.setPlayers;
 
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("메인");
+    }
 }
 
 
