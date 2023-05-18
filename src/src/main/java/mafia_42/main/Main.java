6de2cd4197076962ac45548267d09d57a8c22316
JulienJang <big00124@naver.com>
2023-05-19 00:03:13 +0900

    게임에 참가할 인원 받고 이름 입력받기

--- src/src/main/java/mafia_42/main/Main.java
+++ src/src/main/java/mafia_42/main/Main.java
@@ -9,5 +9,23 @@ import mafia_42.player.Police;
 import java.util.*;
 import java.lang.*;
 
+import static mafia_42.game.Game.setPlayers;
+
 public class Main {
+    public static void main(String[] args) {
+        //필요한 클래스 로드
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        Game game = new Game();
+        AsciiArtPrinter.startLogo();
+
+        //참가자 인원 수 입력하기
+        int numOfPlayers = game.setNumberOfPeople();
+
+        // 참가자 이름 입력 받기
+        ArrayList<String> players = setPlayers(numOfPlayers);
+        ArrayList<String> deadPlayers = new ArrayList<>();
+        ArrayList<String> citizenTeam = new ArrayList<>();
+        ArrayList<String> mafiaTeam = new ArrayList<>();
+    }
 }
