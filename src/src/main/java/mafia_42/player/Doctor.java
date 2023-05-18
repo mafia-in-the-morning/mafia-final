5ee93db03961bf86f8e2b6ca2225af9ca4b7984a
JulienJang <big00124@naver.com>
2023-05-19 02:06:52 +0900

    나머지 부분 최종 커밋-고지혜

--- src/src/main/java/mafia_42/player/Doctor.java
+++ src/src/main/java/mafia_42/player/Doctor.java
@@ -1,7 +1,9 @@
 package mafia_42.player;
+
 import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Doctor extends Player{
     Scanner scanner = new Scanner(System.in);
     public Doctor(String name) {
@@ -9,28 +11,33 @@ public class Doctor extends Player{
         super(name);
     }
 
-    public Player selectTarget(ArrayList<Player> players, ArrayList<Player> deadPlayers, Doctor doctor) {
-        Player doctorTarget = null;
-        if (!deadPlayers.contains(doctor)) {
-            public String selectPatient(ArrayList<String> players, ArrayList<String> deadPlayers, Doctor doctor){
-                String doctorTarget = "";
-                if (!deadPlayers.contains(doctor.getName())) {
-                    do {
-                        System.out.print("의사는 누구를 살리겠습니까? ");
-                        for (Player player : players) {
-                            System.out.println("\n현재 살아있는 인원 : " + player.getName());
-                        }
-                        doctorTarget = new Player(scanner.nextLine());
-                        doctorTarget = scanner.nextLine();
-                        if (deadPlayers.contains(doctorTarget)) {
-                            System.out.println(doctorTarget + "님은 이미 죽은 상태입니다. 다시 선택해주세요.");
-                        } else {
-                            System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
-                        }
-                    } while (!players.contains(doctorTarget) || deadPlayers.contains(doctorTarget));
-                } else {
-                    System.out.println("의사가 이미 사망했으므로 의사는 역할을 수행할 수 없습니다.");
+    public String selectPatient(ArrayList<String> players, ArrayList<String> deadPlayers, Doctor doctor){
+        String doctorTarget = "";
+        if (!deadPlayers.contains(doctor.getName())) {
+            do {
+                System.out.print("🩺의사는 누구를 살리겠습니까? ");
+                System.out.print("\n현재 살아있는 인원: [ ");
+                for (String value : players) {
+                    System.out.print(value + " ");
                 }
-                return doctorTarget;
-            }
+
+                System.out.println("] ");
+                doctorTarget = scanner.nextLine();
+                if (players.contains(doctorTarget) && !deadPlayers.contains(doctorTarget)) {
+                    break;
+                }
+                else if (deadPlayers.contains(doctorTarget)) {
+                    System.out.println(">>" + doctorTarget + "님은 이미 죽은 상태입니다. 다시 선택해주세요.");
+                    System.out.println(" ");
+                } else {
+                    System.out.println(">> 잘못된 입력입니다. 다시 입력해주세요.");
+                    System.out.println(" ");
+                }
+            } while (!players.contains(doctorTarget) || deadPlayers.contains(doctorTarget));
+        } else {
+            System.out.println(">> ☠️의사가 이미 사망했으므로 의사는 역할을 수행할 수 없습니다.");
+            System.out.println(" ");
         }
+        return doctorTarget;
+    }
+}
